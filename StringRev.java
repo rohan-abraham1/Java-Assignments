da26c5470f8d8d94f6f2ac818a4774a10117ec4d
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-10 01:15:14 +0530

    Code to reverse added

--- StringRev.java
+++ StringRev.java
@@ -5,11 +5,22 @@ public class StringRev {
 	public static void main (String[] args) {
 		Scanner input = new Scanner(System.in);
 		char temp;
+		int i;
 		System.out.println("Enter a string: ");
 		String str = input.nextLine();
-		char[] strArray = new char[str.length()];
-		for (int i=0; i<str.length(); i++) {
+		int n = str.length();
+		char[] strArray = new char[n];
+		for (i=0; i<n; i++) {
             strArray[i] = str.charAt(i);
+		}
+		for (i=0;i<n;i++) {
+			temp = strArray[i];
+			strArray[i] = strArray[n-i-1];
+			strArray[n-i-1] = temp;
+		}
+		System.out.print("Reversed String is: ");
+		for (i=0;i<n;i++) {
+			System.out.print(strArray[i]);
 		}
 	}
 }
