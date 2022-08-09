9ce84750c1acd836e80aaa2b09c340314210b11a
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-10 01:05:11 +0530

    Converted string to char array

--- StringRev.java
+++ StringRev.java
@@ -4,7 +4,12 @@ import java.util.Scanner;
 public class StringRev {
 	public static void main (String[] args) {
 		Scanner input = new Scanner(System.in);
+		char temp;
 		System.out.println("Enter a string: ");
 		String str = input.nextLine();
+		char[] strArray = new char[str.length()];
+		for (int i=0; i<str.length(); i++) {
+            strArray[i] = str.charAt(i);
+		}
 	}
 }
