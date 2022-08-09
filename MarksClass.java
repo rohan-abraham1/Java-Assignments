762da17fb3ec1e06aab08f14d79bd0159ddf431c
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-09 16:19:39 +0530

    Code to check for amazing added

--- MarksClass.java
+++ MarksClass.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 public class MarksClass {
 	public static void main (String[] args) {
 		Scanner input = new Scanner(System.in);
-		int mark;
 		System.out.println("Enter Marks: ");
-		mark = input.nextInt();
+		int mark = input.nextInt();
+		if (mark >= 90) {
+			System.out.println("Amazing");
+		}
+		input.close();
 	}
 }
