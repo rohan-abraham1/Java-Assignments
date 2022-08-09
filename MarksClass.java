3bb2790ed1a5191442410b541b49e950064321d6
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-09 15:49:29 +0530

    Code to accept input created

--- MarksClass.java
+++ MarksClass.java
@@ -1,5 +1,11 @@
 package com.orthofx;
+import java.util.Scanner;
 
 public class MarksClass {
-	
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		int mark;
+		System.out.println("Enter Marks: ");
+		mark = input.nextInt();
+	}
 }
