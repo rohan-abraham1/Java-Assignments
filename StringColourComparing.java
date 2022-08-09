42717e9db241f130eacc1a33834192d7f096ce3f
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-09 20:55:50 +0530

    Code to input string added

--- StringColourComparing.java
+++ StringColourComparing.java
@@ -1,5 +1,10 @@
 package com.orthofx;
+import java.util.Scanner;
 
 public class StringColourComparing {
-
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String str = input.nextLine();
+	}
 }
