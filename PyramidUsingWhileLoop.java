22510794f84cc128f0b099fd19ac46088106c062
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-10 00:32:36 +0530

    Code to read value of N

--- PyramidUsingWhileLoop.java
+++ PyramidUsingWhileLoop.java
@@ -1,5 +1,10 @@
 package com.orthofx;
+import java.util.Scanner;
 
 public class PyramidUsingWhileLoop {
-
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the Value of N: ");
+		int n = input.nextInt();
+	}
 }
