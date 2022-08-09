0d4e5330d884e3a6b7da06d41d85d46d8361f61f
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-10 00:27:43 +0530

    Code to print pattern added

--- PyramidUsingLoop.java
+++ PyramidUsingLoop.java
@@ -6,6 +6,14 @@ public class PyramidUsingLoop {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter Value of N: ");
 		int n = input.nextInt();
+		for (int i=0;i<=n;i++) {
+			for (int j=0;j<=n-i-1;j++) {
+				System.out.print(" ");
+			}
+			for (int k=0;k<2*i-1;k++) {
+				System.out.print("*");
+			}	
+			System.out.print('\n');
+		}
 	}
-
 }
