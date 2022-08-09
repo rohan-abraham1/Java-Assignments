4dbfe52be13472e8977d57f1465767835dce246b
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-09 23:31:06 +0530

    Accepted Input

--- PyramidUsingLoop.java
+++ PyramidUsingLoop.java
@@ -1,5 +1,11 @@
 package com.orthofx;
+import java.util.Scanner;
 
 public class PyramidUsingLoop {
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter Value of N: ");
+		int n = input.nextInt();
+	}
 
 }
