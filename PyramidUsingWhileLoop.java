9044a4a9c26f210790c6d6d8a1e9c75c6dc4aebd
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-10 00:36:32 +0530

    Code to print pattern

--- PyramidUsingWhileLoop.java
+++ PyramidUsingWhileLoop.java
@@ -6,5 +6,16 @@ public class PyramidUsingWhileLoop {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the Value of N: ");
 		int n = input.nextInt();
+		int i=0;
+		while (i<=n) {
+			for (int j=0;j<=n-i-1;j++) {
+				System.out.print(" ");
+			}
+			for (int k=0;k<2*i-1;k++) {
+				System.out.print("*");
+			}	
+			System.out.print('\n');
+			i++;
+		}
 	}
 }
