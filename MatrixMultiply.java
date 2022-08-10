452b476d1b063db3cf9595ebbf4afb6fb15b9841
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-11 01:14:08 +0530

    Update MatrixMultiply.java
    Code to multiply added

--- MatrixMultiply.java
+++ MatrixMultiply.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class MatrixMultiply {
 	public static void main (String[] args) {
-		int m, n, p, q, i, j;
+		int m, n, p, q, i, j, k;
 		Scanner input = new Scanner(System.in);
 	    input = new Scanner(System.in);
 	    System.out.println("Enter the number of rows of the matrix 1:");
@@ -23,6 +23,25 @@ public class MatrixMultiply {
 	    System.out.println("Enter the elements of matrix 2:");
 	    for (i = 0; i < p; i++)
 	    	for (j = 0; j < q; j++)
-	    		matrix2[i][j] = input.nextInt();    
+	    		matrix2[i][j] = input.nextInt();
+	    if (m != q)
+            System.out.println("Multiplication Not Possible");
+	    else
+	    {
+	    	int matrix3[][] = new int[m][q];
+	    	for (i = 0; i < m; i++) {
+	            for (j = 0; j < q; j++) {
+	                for (k = 0; k < p; k++)
+	                	matrix3[i][j] += matrix1[i][k] * matrix2[k][j];
+	            	}
+	            }
+	    	System.out.println("\nResultant Matrix:");
+	    	for(i=0;i<m;i++){    
+		    	for(j=0;j<n;j++){    
+		    		System.out.print(matrix3[i][j]+" ");    
+		    	}    
+		    	System.out.println();
+		    	} 
+	        }
 	}
 }
