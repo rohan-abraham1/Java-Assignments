2e551f5d5768fd30cc7f2ba3e58db14d3acd1ce9
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-11 01:20:46 +0530

    Update MatrixMultiplyWithConstant.java
    Code to accept matrix

--- MatrixMultiplyWithConstant.java
+++ MatrixMultiplyWithConstant.java
@@ -1,1 +1,17 @@
+package com.orthofx;
+import java.util.Scanner;
 
+public class MatrixMultiplyWithConstant {
+	public static void main (String[] args) {
+		int m, n, s, i, j;
+		Scanner input = new Scanner(System.in);
+	    input = new Scanner(System.in);
+	    System.out.println("Enter the number of rows of the matrix:");
+	    m = input.nextInt();
+	    System.out.println("Enter the number of columns of the matrix:");
+	    n = input.nextInt();
+	    int matrix[][] = new int[m][n];
+	    System.out.println("Enter the Multiplication Value : ");
+	    s = input.nextInt();
+	}
+}
