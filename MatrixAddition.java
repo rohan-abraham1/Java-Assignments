74c840591652d184d016995b9e85221fce500ded
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-11 00:52:36 +0530

    Update MatrixAddition.java
    Code to accept matrices added

--- MatrixAddition.java
+++ MatrixAddition.java
@@ -1,1 +1,25 @@
+package com.orthofx;
+import java.util.Scanner;
+
+public class MatrixAddition {
+	public static void main (String[] args) {
+		int m, n, i, j;
+		Scanner input = new Scanner(System.in);
+	    input = new Scanner(System.in);
+	    System.out.println("Enter the number of rows of the matrix:");
+	    m = input.nextInt();
+	    System.out.println("Enter the number of columns of the matrix:");
+	    n = input.nextInt();
+	    int matrix1[][] = new int[m][n];
+	    int matrix2[][] = new int[m][n];
+	    System.out.println("Enter the elements of matrix 1:");
+	    for (i = 0; i < m; i++)
+	    	for (j = 0; j < n; j++)
+	    		matrix1[i][j] = input.nextInt();
+	    System.out.println("Enter the elements of matrix 2:");
+	    for (i = 0; i < m; i++)
+	    	for (j = 0; j < n; j++)
+	    		matrix2[i][j] = input.nextInt();
+	}
+}
 
