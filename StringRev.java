aefb17830910194dcf47c563b9ac638a8cb9fbd7
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-11 00:29:38 +0530

    Code Optimized

--- StringRev.java
+++ StringRev.java
@@ -4,23 +4,18 @@ import java.util.Scanner;
 public class StringRev {
 	public static void main (String[] args) {
 		Scanner input = new Scanner(System.in);
-		char temp;
 		int i;
 		System.out.println("Enter a string: ");
 		String str = input.nextLine();
 		int n = str.length();
 		char[] strArray = new char[n];
+		char[] strArray2 = new char[n];
 		for (i=0; i<n; i++) {
             strArray[i] = str.charAt(i);
 		}
-		for (i=0;i<n;i++) {
-			temp = strArray[i];
-			strArray[i] = strArray[n-i-1];
-			strArray[n-i-1] = temp;
-		}
-		System.out.print("Reversed String is: ");
-		for (i=0;i<n;i++) {
-			System.out.print(strArray[i]);
-		}
+		strArray2 = strArray;
+		for (i=0;i<n;i++)
+            {strArray[i] = strArray2[n-i-1];}
+        System.out.println(new String(strArray));
 	}
 }
