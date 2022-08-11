341056f3950c3ffbad58b4f99e59e65f06d32aeb
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-11 23:40:09 +0530

    function to return int value of roman charecter added

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -3,6 +3,33 @@ import java.util.Scanner;
 
 public class RomanToInteger {
 	
+	 int value (char s) {
+		if (s=='I') {
+			return 1; 
+		}
+		else if (s=='V') {
+			return 5; 
+		}
+		else if (s=='X') {
+			return 10; 
+		}
+		else if (s=='L') {
+			return 50; 
+		}
+		else if (s=='C') {
+			return 100; 
+		}
+		else if (s=='D') {
+			return 500; 
+		}
+		else if (s=='M') {
+			return 1000; 
+		}
+		else {
+			return -1;
+		}
+	}
+	
 	public static void main(String[] args) {
 		int i,n;
 		Scanner input = new Scanner(System.in);
