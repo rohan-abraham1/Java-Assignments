31230491386638e3ac8f5e93c67d35c7b5eda635
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-11 23:36:34 +0530

    Code to accept roman value as string and converted to char array

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -1,1 +1,19 @@
+package com.orthofx;
+import java.util.Scanner;
 
+public class RomanToInteger {
+	
+	public static void main(String[] args) {
+		int i,n;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter Roman Numeral Number:");
+		String str = input.nextLine();
+		n = str.length();
+		char[] strArray = new char[n+1];
+		for (i=0; i<n; i++) {
+            strArray[i] = str.charAt(i);
+		}
+	}
+	
+	 
+}
