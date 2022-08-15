443b5b569a1d6d5b3406cbeb56ffb90d486b6293
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-15 20:07:06 +0530

    Code to initate map

--- Array List/ArrayListAddition.java
+++ Array List/ArrayListAddition.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class ArrayListAddition {
-
+	public static void main (String[] args) {
+		ArrayList<Integer[][]> row = new ArrayList<Integer[][]>();
+		int m, n, i, j;
+		Scanner input = new Scanner(System.in);
+	    input = new Scanner(System.in);
+	    System.out.println("Enter the number of rows of the matrix:");
+	    m = input.nextInt();
+	    System.out.println("Enter the number of columns of the matrix:");
+	    n = input.nextInt();
+	    ArrayList<ArrayList<Integer>> mat = new ArrayList<>(n);
+	}
 }
 
--- Array List/StudentMark.java
+++ Array List/StudentMark.java
@@ -2,5 +2,19 @@ package com.orthofx;
 import java.util.Scanner;
 
 public class StudentMark {
-
+	public static void main() {
+	HashMap<String, Integer> map = new HashMap<>();	
+	int i, n, mark;
+	String name;
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter number of students: ");
+	n = input.nextInt();
+	for (i=0;i<n;i++) {
+		System.out.println("Enter name of student " + i+1 + " :");
+		name = input.nextLine();
+		System.out.println("Enter marks of student " + i+1 + " :");
+		mark = input.nextInt();
+		map.put(name, mark);
+		}
+	}	
 }
