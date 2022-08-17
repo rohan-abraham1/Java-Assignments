aff1728663465a8dd8f5f72f6a13298e90e45f21
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-17 08:16:54 +0530

    Row coloumn value accepted

--- Array List/ArrayListMultiplication.java
+++ Array List/ArrayListMultiplication.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class ArrayListMultiplication {
-
+	public static void main (String[] args) {
+		int m, n, p, q, i, j, k;
+		Scanner input = new Scanner(System.in);
+	    input = new Scanner(System.in);
+	    System.out.println("Enter the number of rows of the matrix 1:");
+	    m = input.nextInt();
+	    System.out.println("Enter the number of columns of the matrix 1:");
+	    n = input.nextInt();
+	    System.out.println("Enter the number of rows of the matrix 2:");
+	    p = input.nextInt();
+	    System.out.println("Enter the number of columns of the matrix 2:");
+	    q = input.nextInt();
+	    ArrayList<ArrayList<Integer>> mat1 = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> mat2 = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> mul = new ArrayList<ArrayList<Integer>>();
+	}
 }
 
