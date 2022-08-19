369b0e98a97621eb75a910276cf8641bdc026284
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-19 17:58:45 +0530

    Update ForestShooting.java
    Completed

--- Forest Shooting/ForestShooting.java
+++ Forest Shooting/ForestShooting.java
@@ -1,1 +1,60 @@
+package com.orthofx;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ForestShooting {
+
+	static ArrayList<Integer> line = new ArrayList<Integer>();
+	static int flag, n, i;
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter number of people: ");
+		n = input.nextInt();
+		for (i = 1; i <= n; i++) {
+			line.add(i);
+		}
+		fireOne();
+	}
+
+	static void fireOne() {
+		n = line.get(line.size()-1);
+		for (i = 0; i < line.size(); i ++) {
+			line.remove(i);
+		}
+		System.out.println(line);
+		if (line.size() == 1) {
+			System.out.println(line);
+			System.exit(0);
+		}
+		else {
+			if (line.get(line.size()-1) == n) {
+				fireOne();
+			}
+			else {
+				fireTwo();
+			}
+		}	
+	}
+	
+	static void fireTwo() {
+		n = line.get(line.size()-1);
+		for (i = 1; i < line.size(); i ++) {
+			line.remove(i);
+		}
+		System.out.println(line);
+		if (line.size() == 1) {
+			System.out.println(line);
+			System.exit(0);
+		}
+		else {
+			if (line.get(line.size()-1) == n) {
+				fireOne();
+			}
+			else {
+				fireTwo();
+			}
+		}
+	}
+}
