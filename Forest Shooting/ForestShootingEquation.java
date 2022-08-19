c28da517c7951b9e6863078bc6869f2e2d679658
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-19 17:58:19 +0530

    Update ForestShootingEquation.java
    Completed

--- Forest Shooting/ForestShootingEquation.java
+++ Forest Shooting/ForestShootingEquation.java
@@ -1,1 +1,17 @@
+package com.orthofx;
 
+import java.util.Scanner;
+
+public class ForestShootingEquation {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n, pos, low = 2;
+		System.out.println("Enter number of people: ");
+		n = input.nextInt();
+		do {
+			low *= 2;
+		} while(n>low);
+		low /= 2;
+		System.out.println((n - low)*2);
+	}
+}
