c188181bd3d0983caa9134dd5ed1a6505a1fdb8c
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-08-23 16:36:49 +0530

    Update Car.java
    Updates made to data variables

--- Vehicle Class/Car.java
+++ Vehicle Class/Car.java
@@ -6,33 +6,33 @@ import java.util.Scanner;
 
 public class Car extends Vehicle {
   char choice;
-	String fuelType, transmission, numberOfDoors, value;
+	String fuelType, transmission, numberOfDoors;
 
 	HashMap<String, ArrayList<String>> carList = new HashMap<String, ArrayList<String>>();
   
   	public void addCarDetails() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter Model Name: ");
-		car.model = input.next();
+		model = input.next();
 		
 		if (!carList.containsKey(car.model)) {
-			carList.put(car.model, new ArrayList<String>());
+			carList.put(model, new ArrayList<String>());
 
 			System.out.print("Enter Colour: ");
-			car.colour = input.next();
-			carList.get(car.model).add(car.colour);
+			colour = input.next();
+			carList.get(model).add(colour);
 
 			System.out.print("Enter Type of Fuel: ");
 			car.fuelType = input.next();
-			carList.get(car.model).add(car.fuelType);
+			carList.get(model).add(fuelType);
 
 			System.out.print("Enter Transmission type: ");
 			car.transmission = input.next();
-			carList.get(car.model).add(car.transmission);
+			carList.get(model).add(transmission);
 
 			System.out.print("Enter Number of Doors: ");
 			car.numberOfDoors = input.next();
-			carList.get(car.model).add(car.numberOfDoors);
+			carList.get(model).add(numberOfDoors);
 		}
 		else
 			System.out.println("Entered Model aldready exists!");
@@ -42,20 +42,20 @@ public class Car extends Vehicle {
 	public void displayCarDetails() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter Model Name: ");
-		car.model = input.next();
+		model = input.next();
 
 		if (carList.containsKey(car.model)) {
 			System.out.print("Colour is: ");
-			System.out.println(carList.get(car.model).get(0));
+			System.out.println(carList.get(model).get(0));
 
 			System.out.print("Type of Fuel is: ");
-			System.out.println(carList.get(car.model).get(1));
+			System.out.println(carList.get(model).get(1));
 
 			System.out.print("Transmission type is: ");
-			System.out.println(carList.get(car.model).get(2));
+			System.out.println(carList.get(model).get(2));
 
 			System.out.print("Number of Doors is: ");
-			System.out.println(carList.get(car.model).get(3));
+			System.out.println(carList.get(model).get(3));
 		}
 		else
 			System.out.println("Invalid Model name");
@@ -65,39 +65,39 @@ public class Car extends Vehicle {
 	public void editCarDetails() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter Model Name: ");
-		car.model = input.next();
+		model = input.next();
 
-		if (carList.containsKey(car.model)) {
+		if (carList.containsKey(model)) {
 			System.out.print("Do you want to edit colour?(Y/N): ");
 			choice = input.next().charAt(0);
 			if (choice == 'Y' || choice == 'y') {
 				System.out.print("Enter new colour: ");
-				value = input.next();
-				carList.get(car.model).set(0, car.value);
+				colour = input.next();
+				carList.get(model).set(0, colour);
 			}
 
 			System.out.print("Do you want to edit type of fuel?(Y/N): ");
 			choice = input.next().charAt(0);
 			if (choice == 'Y' || choice == 'y') {
 				System.out.print("Enter new fuel type: ");
-				value = input.next();
-				carList.get(car.model).set(1, car.value);
+				fuelType = input.next();
+				carList.get(model).set(1, fuelType);
 			}
 
 			System.out.print("Do you want to edit transmission type?(Y/N): ");
 			choice = input.next().charAt(0);
 			if (choice == 'Y' || choice == 'y') {
 				System.out.print("Enter new transmission: ");
-				value = input.next();
-				carList.get(car.model).set(2, car.value);
+				transmission = input.next();
+				carList.get(model).set(2, transmission);
 			}
 
 			System.out.print("Do you want to edit number of doors?(Y/N): ");
 			choice = input.next().charAt(0);
 			if (choice == 'Y' || choice == 'y') {
 				System.out.print("Enter new number of doors: ");
-				value = input.next();
-				carList.get(car.model).set(3, car.value);
+				numberOfDoors = input.next();
+				carList.get(model).set(3, numberOfDoors);
 			}
 		}
 		else
@@ -107,10 +107,10 @@ public class Car extends Vehicle {
 	public void removeCarDetails() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter Model Name: ");
-		car.model = input.next();
+		model = input.next();
 
-		if (carList.containsKey(car.model)) {
-			carList.remove(car.model);
+		if (carList.containsKey(model)) {
+			carList.remove(model);
 			System.out.print("Data deleted");
 		}
 		else
